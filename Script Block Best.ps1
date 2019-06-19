f723f097d002c52f6b8db4c6747e744a3e788335
jfurcht21 <51976792+jfurcht21@users.noreply.github.com>
2019-06-18 22:49:22 -0700

    Add files via upload

--- Script Block Best.ps1
+++ Script Block Best.ps1
@@ -1,6 +1,9 @@
-﻿#The $userLoc variable will prompt the user for a City and State, and then store the user's input. example: Tacoma WA
+﻿function Get-Weather {
+
+#The $userLoc variable will prompt the user for a City and State, and then store the user's input. example: Tacoma WA
 $userLoc = read-host -prompt 'Please enter a city and state'
-$userDays = read-host -prompt 'please enter number of days for forecast'
+$userDays = read-host -prompt 'Please enter number of days for forecast'
+$userUnit = read-host -prompt 'Please enter unit: C or F'
 $int = [int] $userDays
 
 #The $formatLoc variable takes the users input, replaces any spaces with '+', and stores that value. example: Tacoma+WA
@@ -9,7 +12,7 @@ $forecastSeconds = $int * 86400
 
 #Adds the location into the url for the API
 $googleGeoJSON = "https://maps.googleapis.com/maps/api/geocode/json?address=" + $formatloc + "&key=AIzaSyDGT-1IEg0NHsXiLAtntLxAFctMttHwTao"
-$Darkskydata = "https://api.darksky.net/forecast/93dcf96ac1507b0bc9c572e244f7b633/" + $varLat + "," + $varLng + "," + $forecastSeconds
+$Darkskydata = "https://api.darksky.net/forecast/93dcf96ac1507b0bc9c572e244f7b633/" + $varLat + "," + $varLng + "," + $forecastSeconds + ",[us]"
 #Invoke URL
 $resultsLocJSON = Invoke-RestMethod $googleGeoJSON
 $resultsDayJSON = Invoke-RestMethod $Darkskydata -Method Get 
@@ -20,11 +23,18 @@ $varLng = $resultsLocJSON.results.geometry.location.lng #This will be your Longi
 
 
 while ($Countdown = $forecastSeconds - 86400 -ge 0){
-    echo $resultsDayJSON.currently.temperature
+    
     $Countdown = ($forecastSeconds -= 86400) 
     $newResults = "https://api.darksky.net/forecast/93dcf96ac1507b0bc9c572e244f7b633/" + $varLat + "," + $varLng + "," + $Countdown
     $Final = Invoke-RestMethod $newResults -Method Get
-    echo $final
     echo (get-Date).AddSeconds($countdown) | Get-Date -Format "dd-MMM-yyyy"
+    write-host "Summary:" $final.currently.summary
+    echo "Average Temperature:" $final.currently.temperature
+    echo "Precipitation Probability:" $final.currently.precipProbability
+    echo "Wind Speed:" $final.currently.windSpeed
+    echo "Humidity:" $final.currently.humidity
+    echo `n
+    
     }
 
+}
