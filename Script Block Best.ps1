d1a157174ca084ad604fe6c236020e98d608cdec
Evan Nave <30371973+navevan@users.noreply.github.com>
2019-06-18 18:05:45 -0700

    Update Script Block Best.ps1

--- Script Block Best.ps1
+++ Script Block Best.ps1
@@ -1,17 +1,6 @@
-﻿#The $userLoc variable will prompt the user for a City and State, and then store the user's input. example: Tacoma WA
+#The $userLoc variable will prompt the user for a City and State, and then store the user's input. example: Tacoma WA
 $userLoc = read-host -prompt 'Please enter a city and state'
-$userDays = read-host -prompt 'please enter number of days for forecast'
-$int = [int] $userDays
 
-#The $formatLoc variable takes the users input, replaces any spaces with '+', and stores that value. example: Tacoma+WA
-$formatLoc = $userLoc -replace(' ', '+') 
-$forecastSeconds = $int * 86400
-
-#Adds the location into the url for the API
-$googleGeoJSON = "https://maps.googleapis.com/maps/api/geocode/json?address=" + $formatloc + "&key=AIzaSyDGT-1IEg0NHsXiLAtntLxAFctMttHwTao"
-$Darkskydata = "https://api.darksky.net/forecast/93dcf96ac1507b0bc9c572e244f7b633/" + $varLat + "," + $varLng + "," + $forecastSeconds
-#Invoke URL
-$resultsLocJSON = Invoke-RestMethod $googleGeoJSON
 $resultsDayJSON = Invoke-RestMethod $Darkskydata -Method Get 
 
 #Sets the two variables to equal the latitude/longitude properties
