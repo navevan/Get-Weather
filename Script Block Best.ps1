f793c049a0ace25eb4d7d5b2a19ee7f5743dae21
Evan Nave <30371973+navevan@users.noreply.github.com>
2019-06-19 20:14:39 -0700

    Update Script Block Best.ps1

--- Script Block Best.ps1
+++ Script Block Best.ps1
@@ -1,40 +1,82 @@
-﻿function Get-Weather {
+function Get-Weather {
 
 #The $userLoc variable will prompt the user for a City and State, and then store the user's input. example: Tacoma WA
 $userLoc = read-host -prompt 'Please enter a city and state'
 $userDays = read-host -prompt 'Please enter number of days for forecast'
 $userUnit = read-host -prompt 'Please enter unit: C or F'
+
+#Declarations of input
 $int = [int] $userDays
+$temp = [char] $userunit
 
 #The $formatLoc variable takes the users input, replaces any spaces with '+', and stores that value. example: Tacoma+WA
 $formatLoc = $userLoc -replace(' ', '+') 
-$forecastSeconds = $int * 86400
+
 
 #Adds the location into the url for the API
 $googleGeoJSON = "https://maps.googleapis.com/maps/api/geocode/json?address=" + $formatloc + "&key=AIzaSyDGT-1IEg0NHsXiLAtntLxAFctMttHwTao"
-$Darkskydata = "https://api.darksky.net/forecast/93dcf96ac1507b0bc9c572e244f7b633/" + $varLat + "," + $varLng + "," + $forecastSeconds + ",[us]"
-#Invoke URL
+
+
+#Invoke geolocation URL
 $resultsLocJSON = Invoke-RestMethod $googleGeoJSON
-$resultsDayJSON = Invoke-RestMethod $Darkskydata -Method Get 
+
 
 #Sets the two variables to equal the latitude/longitude properties
 $varLat = $resultsLocJSON.results.geometry.location.lat #This will be your Latitude
 $varLng = $resultsLocJSON.results.geometry.location.lng #This will be your Longitude
 
+#convert number of days requested into proper format
+$daysrequested = $int * 86400 #This takes users request of days and converts into unix
+$daysforcasted = [int][double]::Parse((Get-Date (get-date).touniversaltime() -UFormat %s)) #This converts todays date into Unix
+$newtime = $daysforcasted + $daysrequested #Adds user request and todays date
 
-while ($Countdown = $forecastSeconds - 86400 -ge 0){
+#Function to convert UNIX back into date
+Function get-epochDate ($epochDate) { [timezone]::CurrentTimeZone.ToLocalTime(([datetime]'1/1/1970').AddSeconds($epochDate)) } #converts unix back into datetime
+
+#Adds $varLat, $varLng and converted time into Weather API
+$Darkskydata = "https://api.darksky.net/forecast/93dcf96ac1507b0bc9c572e244f7b633/" + $varLat + "," + $varLng + "," + $newtime +"?" + ",[us]"
+
+#Invoke weather URL
+$resultsDayJSON = Invoke-RestMethod $Darkskydata -Method Get 
+
+#Function to convert F to C
+function get-f2c([double] $farentemp)
+{
+    $celtemp = $fahrentemp - 32
+    $celtemp = $celtemp / 1.8
+    $celtemp
+}
+
+#Display for prototype/Troubleshooting purposes
+while ($Countdown = $daysforcasted + 86400 -le $newtime){
     
-    $Countdown = ($forecastSeconds -= 86400) 
+    $Countdown = ($daysforcasted += 86400) 
     $newResults = "https://api.darksky.net/forecast/93dcf96ac1507b0bc9c572e244f7b633/" + $varLat + "," + $varLng + "," + $Countdown
     $Final = Invoke-RestMethod $newResults -Method Get
-    echo (get-Date).AddSeconds($countdown) | Get-Date -Format "dd-MMM-yyyy"
-    write-host "Summary:" $final.currently.summary
+    $confirmtime = [int] $final.currently.time
+
+#Broken attempt within the while loop to do a conditional for F or C
+if ($temp = "F"){
+    get-epochDate $confirmtime
+    echo  "your punk ass time zone is: "$final.timezone
+    write-host "Summary:" $final.hourly.summary
     echo "Average Temperature:" $final.currently.temperature
     echo "Precipitation Probability:" $final.currently.precipProbability
     echo "Wind Speed:" $final.currently.windSpeed
     echo "Humidity:" $final.currently.humidity
-    echo `n
-    
-    }
+    echo `n    }
 
+else { 
+    get-epochDate $confirmtime
+    echo  "your punk ass time zone is: "$final.timezone
+    write-host "Summary:" $final.hourly.summary
+    $farentemp = [double] $final.currently.temperature
+    get-f2c $farentemp
+    echo "Precipitation Probability:" $final.currently.precipProbability
+    echo "Wind Speed:" $final.currently.windSpeed
+    echo "Humidity:" $final.currently.humidity
+    echo `n
+
+     }
+                                                       } 
 }
